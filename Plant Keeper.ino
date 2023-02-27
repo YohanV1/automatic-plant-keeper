195c955fdea8595088b1d56f7a196215f69664f9
Yohan Vinu <yohanvvinu@gmail.com>
2023-02-27 17:19:59 +0530

    Add files via upload
    Add .ino file

---
+++ Plant Keeper.ino
@@ -0,0 +1,61 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+int motor = D4;
+int sensor = A0;
+int sens_val;
+int flag = 0;
+
+char auth[] = "KGl0JNpPzQOWeNLvmx8UmotIZ0mS1RK7";
+char ssid[] = "VVAMAPLE";
+char pass[] = "admin2975";
+
+int Relay = 0;
+int automatic = 1;
+
+void setup() {
+  pinMode(motor, OUTPUT);
+  pinMode(sensor, INPUT);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  Serial.begin(9600);
+}
+
+BLYNK_WRITE(V1) {
+  Relay = param.asInt();
+}
+
+BLYNK_WRITE(V2) {
+  automatic = param.asInt();
+}
+
+void loop() {
+
+  sens_val = analogRead(A0);
+
+  if(sens_val > 700 && automatic == 0){
+    Blynk.virtualWrite(V1, Relay);
+    digitalWrite(motor, LOW);
+  }
+  else if(sens_val<=700 && automatic == 0){
+    Blynk.virtualWrite(V1, Relay);
+    digitalWrite(motor, HIGH);
+  }
+  else if (Relay==0 && automatic==1){
+    digitalWrite(motor, LOW);
+  }
+  else if (Relay==1 && automatic==1){
+    digitalWrite(motor, HIGH);
+  }
+
+  int value = sens_val;
+  value = map(value, 0, 1024, 0, 100);
+  value = (value - 100) * -1;
+  Blynk.virtualWrite(V0, value);
+
+  char buffer[40];
+  sprintf(buffer, "Sens_val: %d, value: %d, Relay: %d, automatic: %d", sens_val, value, Relay, automatic);
+  Serial.println(buffer);
+
+  Blynk.run();
+
+}
